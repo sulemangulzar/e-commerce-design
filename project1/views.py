563871d34b31a1922c299b2ad13e4fbea6b72d47
Suleman Gulzar <isulemang@icloud.com>
2024-03-03 22:44:06 +0500

    changed

--- project1/views.py
+++ project1/views.py
@@ -5,12 +5,7 @@ def HomePage(request):
     return render(request,"index.html",)
 def CartPage(request):
     return render(request,"cart.html",)
-def aboutUs(request):
-    return HttpResponse("About Us Page")
-def ContactUs(request):
-    return HttpResponse("Contact Page")
-def Content(request, content):
-    return HttpResponse(content)
+
 
 
 
